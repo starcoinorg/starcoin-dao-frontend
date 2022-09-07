92790d89b2d07ab407ac093a0b752b1eef54cc0d
kkkycocho <kkkycocho@outlook.com>
2022-09-07 10:01:06 +0800

    fix: dao list fetch

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -10,41 +10,19 @@ import MainViewLayout from '../components/mainViewLayout';
 import { useRequest } from '../hooks/useRequest';
 import axios from 'axios';
 import config from '../utils/getConfig';
-import { DaoService } from '../services/daoService';
 
-const url_prev = `${config.api}/`;
 
 const Explore = () => {
   const { theme, resetTheme } = useContext(CustomThemeContext);
   const [daoList, setDaoList] = useState([]);
-  const [daos, setDaos] = useState([]);
-  const [_daos, _setDaos] = useState([]);
-  const [chainDaos, setChainDaos] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const daoService = new DaoService();
 
-  const getAllData = async () => {
-    Promise.all([
-      axios.get(`${url_prev}daos`, {
-        method: 'get',
-        params: {
-          page: '1',
-          size: '1',
-        },
-      }),
-      axios.get(`${url_prev}daos`, {
-        method: 'get',
-        params: {
-          page: '0',
-          size: '1',
-        },
-      }),
-    ]).then(res => {
-      setDaos(res[0].data);
-      _setDaos(res[1].data);
-      setLoading(false);
-    });
-  };
+  const { data: _daoList, loading } = useRequest('daos', {
+    method: 'get',
+    params: {
+      page: '0',
+      size: '100',
+    },
+  });
 
   useEffect(() => {
     if (theme.active) {
@@ -53,23 +31,10 @@ const Explore = () => {
   }, [theme, resetTheme]);
 
   useEffect(() => {
-    getAllData();
-  }, []);
-
-  useEffect(() => {
-    daoService.listDaos().then(res => {
-      console.log('loaded chain daos', res);
-      setChainDaos(res);
-    });
-  }, []);
-
-  useEffect(() => {
-    if (daos && _daos && chainDaos) {
-      const totalDaos = [...daos, ..._daos, ...chainDaos];
-      console.log('loaded total daos', totalDaos);
-      setDaoList(totalDaos);
+    if (_daoList) {
+      setDaoList(_daoList);
     }
-  }, [daos, _daos, chainDaos]);
+  }, [_daoList]);
 
   return (
     <Layout>
