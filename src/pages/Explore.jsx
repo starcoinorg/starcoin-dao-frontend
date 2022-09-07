5826eb144b640309f6a0c30f50dda933373eb99e
kkkycocho <kkkycocho@outlook.com>
2022-09-07 10:24:40 +0800

    fix: explore

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,16 +1,12 @@
 import React, { useEffect, useState, useContext } from 'react';
 
 import { CustomThemeContext } from '../contexts/CustomThemeContext';
-import { ExploreContext } from '../contexts/ExploreContext';
 import ExploreFilters from '../components/exploreFilters';
 import ExploreList from '../components/exploreList';
 import Layout from '../components/layout';
 import Loading from '../components/loading';
 import MainViewLayout from '../components/mainViewLayout';
 import { useRequest } from '../hooks/useRequest';
-import axios from 'axios';
-import config from '../utils/getConfig';
-
 
 const Explore = () => {
   const { theme, resetTheme } = useContext(CustomThemeContext);
@@ -39,13 +35,13 @@ const Explore = () => {
   return (
     <Layout>
       <MainViewLayout header='Explore DAOs'>
-        {!loading ? (
+        {loading ? (
+          <Loading message='Fetching DAOs...' />
+        ) : (
           <>
             <ExploreFilters daoCount={daoList?.length || 0} />
             <ExploreList daoList={daoList} />
           </>
-        ) : (
-          <Loading message='Fetching DAOs...' />
         )}
       </MainViewLayout>
     </Layout>
