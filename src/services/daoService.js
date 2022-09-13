9adab0003064b0647a84e814f54e1e6bf6d344aa
kkkycocho <kkkycocho@outlook.com>
2022-09-13 21:37:20 +0800

    fix: await mkdir

--- src/services/daoService.js
+++ src/services/daoService.js
@@ -12,7 +12,7 @@ class DaoService {
 
   async createDao(cfg) {
     // download starcoin framework
-    this.wasmfs.fs.mkdirpSync('./workspace/starcoin-framework/unit-test');
+    await this.wasmfs.fs.mkdirpSync('/workspace/starcoin-framework/unit-test');
 
     // const starcoinFrameworkURL =
     //   process.env.NODE_ENV === 'production'
@@ -21,44 +21,46 @@ class DaoService {
     const starcoinFrameworkURL = '/data/starcoin-framework.zip';
     await this.git.download(
       starcoinFrameworkURL,
-      './workspace/starcoin-framework',
+      '/workspace/starcoin-framework',
     );
 
-    const dirTree = this.wasmfs.toJSON('./workspace/starcoin-framework');
+    const dirTree = await this.wasmfs.toJSON('/workspace/starcoin-framework');
     console.log(dirTree);
 
     // download starcoin framework
 
-    this.wasmfs.fs.mkdirpSync('./workspace/freepai-plugin/sources');
+    await this.wasmfs.fs.mkdirpSync('/workspace/freepai-plugin/sources');
     // const freepaiPluginURL =
     //   process.env.NODE_ENV === 'production'
     //     ? '/dapps/data/freepai-plugin.zip'
     //     : '/data/freepai-plugin.zip';
     const freepaiPluginURL = '/data/freepai-plugin.zip';
-    await this.git.download(freepaiPluginURL, './workspace/freepai-plugin');
+    await this.git.download(freepaiPluginURL, '/workspace/freepai-plugin');
 
     // render DAO package
-    this.renderDAOPackage('./workspace/my-dao', cfg);
+    this.renderDAOPackage('/workspace/my-dao', cfg);
 
     const mp = new MovePackage(this.wasmfs, {
-      packagePath: './workspace/my-dao',
+      packagePath: '/workspace/my-dao',
       test: false,
       alias: new Map([
-        ['StarcoinFramework', './workspace/starcoin-framework'],
-        ['FreePlugin', './workspace/freepai-plugin'],
+        ['StarcoinFramework', '/workspace/starcoin-framework'],
+        ['FreePlugin', '/workspace/freepai-plugin'],
       ]),
       initFunction: `${cfg.address}::${cfg.name}::initialize`,
     });
 
     await mp.build();
-    const blobBuf = this.wasmfs.fs.readFileSync(
-      './workspace/my-dao/target/starcoin/release/package.blob',
+    console.log('buildddddddddddd');
+    const blobBuf = await this.wasmfs.fs.readFileSync(
+      '/workspace/my-dao/target/starcoin/release/package.blob',
     );
+    console.log('>?>>>>>>>>', blobBuf);
     return blobBuf;
   }
 
-  renderDAOPackage(destPath, cfg) {
-    this.wasmfs.fs.mkdirpSync(destPath);
+  async renderDAOPackage(destPath, cfg) {
+    await this.wasmfs.fs.mkdirpSync(destPath);
 
     window.console.info(
       'Token Address: ' + cfg.address + '::' + cfg.name + '::' + cfg.name,
@@ -66,13 +68,13 @@ class DaoService {
 
     const moveTomlPath = destPath + '/Move.toml';
     const moveTomlContent = MoveTomlTpl(cfg.name, cfg.address);
-    this.wasmfs.fs.writeFileSync(moveTomlPath, moveTomlContent);
-    window.console.info(moveTomlPath);
-    window.console.info(moveTomlContent);
-    window.console.info();
+    await this.wasmfs.fs.writeFileSync(moveTomlPath, moveTomlContent);
+    // window.console.info(moveTomlPath);
+    // window.console.info(moveTomlContent);
+    // window.console.info();
 
     const sourcesDir = destPath + '/sources';
-    this.wasmfs.fs.mkdirpSync(sourcesDir);
+    await this.wasmfs.fs.mkdirpSync(sourcesDir);
     const myTokenPath = sourcesDir + '/MyDAO.move';
     const myTokenContent = MyDAOSourceTpl(
       cfg.address,
@@ -89,7 +91,7 @@ class DaoService {
       cfg.proposalConfig.min_action_delay,
       cfg.proposalConfig.min_proposal_deposit,
     );
-    this.wasmfs.fs.writeFileSync(myTokenPath, myTokenContent);
+    await this.wasmfs.fs.writeFileSync(myTokenPath, myTokenContent);
     window.console.info(myTokenPath);
     window.console.info(myTokenContent);
     window.console.info();
