a482b2f6690963bc6237d9f6310a2c40423aa312
Owen Wu <yubing744@163.com>
2022-11-09 23:59:12 +0800

    feat: opt DAO list load

--- src/utils/dao.js
+++ src/utils/dao.js
@@ -79,7 +79,11 @@ export const hasMinion = (minions, minionType) => {
   return false;
 };
 
-export const listDaos = async () => {
+export const listDaos = async _opts => {
+  const opts = {
+    ..._opts,
+  };
+
   const daoEntrys = await window.starcoin.request({
     method: 'state.list_resource',
     params: [
@@ -98,7 +102,7 @@ export const listDaos = async () => {
   let daos = [];
   for (const key in daoEntrys.resources) {
     const daoId = key.substring(key.indexOf('<') + 1, key.indexOf('>'));
-    const dao = await getDaoDetail(daoId);
+    const dao = await getDaoDetail(daoId, opts.withPlugins);
     daos.push(dao);
   }
 
@@ -155,7 +159,7 @@ export const getDaoInstalledPlugins = async daoId => {
   return plugins;
 };
 
-export const getDaoDetail = async daoId => {
+export const getDaoDetail = async (daoId, withPlugins = true) => {
   const daoTypeTag = daoId;
   const daoAddress = daoId.substring(0, daoId.indexOf('::'));
   const daoInfo = await getDao(daoAddress);
@@ -194,14 +198,17 @@ export const getDaoDetail = async daoId => {
   }
 
   let plugins = [];
-  let installed_web_plugins = await getDaoInstalledPlugins(daoId);
-  if (installed_web_plugins) {
-    for (const i in installed_web_plugins) {
-      const plugin_info = installed_web_plugins[i];
-      const plugin = await getPluginInfo(plugin_info.pluginType);
 
-      if (plugin && plugin.js_entry_uri) {
-        plugins.push(plugin);
+  if (withPlugins) {
+    let installed_web_plugins = await getDaoInstalledPlugins(daoId);
+    if (installed_web_plugins) {
+      for (const i in installed_web_plugins) {
+        const plugin_info = installed_web_plugins[i];
+        const plugin = await getPluginInfo(plugin_info.pluginType);
+
+        if (plugin && plugin.js_entry_uri) {
+          plugins.push(plugin);
+        }
       }
     }
   }
