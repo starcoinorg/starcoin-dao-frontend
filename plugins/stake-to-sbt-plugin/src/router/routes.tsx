5b7b9df8a1965d29faa16c08d889bfd9fee87e57
sven <sven.tan@icloud.com>
2022-11-21 22:25:47 +0800

    fix ci

--- plugins/stake-to-sbt-plugin/src/router/routes.tsx
+++ plugins/stake-to-sbt-plugin/src/router/routes.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
-
 import {
     Switch,
     Route, Redirect
 } from 'react-router-dom'
-
 import IndexPage from '../pages/index'
 import HomePage from "../pages/home"
 import SettingPage from "../pages/setting"
@@ -17,7 +15,7 @@ const Router = () => {
             <Route exact path="/" component={() => <Redirect to="/home"/>}/>
             <Route exact path="/home" component={() => <IndexPage/>}/>
             <Route exact path="/stake" component={() => <StakePage/>}/>
-            <Route exact path="/list" component={() => <HomePage/>}/>
+            <Route exact psath="/list" component={() => <HomePage/>}/>
             <Route exact path="/setting" component={() => <SettingPage/>}/>
             <Route exact path="/detail/:type" component={() => <TypePage/>}/>
         </Switch>
