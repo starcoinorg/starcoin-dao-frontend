be7723654eac98836c12e0aa99b09aee239d8a69
sven <sven.tan@icloud.com>
2022-12-02 17:19:22 +0800

    修复成员插件
    1. 修改邀请成员列表，对邀请列表排序，当前账户的邀请在第一个。
    2. 修改邀请成员列表，accept 动作只对当前账号的邀请有效。其他要求不限制accept
    3. 修复邀请新成员model 样式。
    4. 修改要求新成员按钮，只对已是成员的账户展示。
    5. 删除所有toast， 对部分依赖toast 作为逻辑提示的，改为页面提示。

--- plugins/member-proposal-plugin/src/components/memberInviteList.tsx
+++ plugins/member-proposal-plugin/src/components/memberInviteList.tsx
@@ -1,58 +1,68 @@
-import React, { useEffect, useState } from 'react';
-import { Flex, Spinner, Button, HStack, Stack, Link } from '@chakra-ui/react';
+import React, {useEffect, useState} from 'react';
+import {Flex, Spinner, Button, HStack, Stack, Link} from '@chakra-ui/react';
 
 import MyMemberInviteCard from './myMemberInviteCard';
-import { listAllOffers, doAccecptOffer } from '../utils/memberPluginAPI';
+import {listAllOffers, doAccecptOffer} from '../utils/memberPluginAPI';
 
-const MyMemberInviteList = ({ daoId }) => {
-  const [offers, setOffers] = useState([]);
-  const [loading, setLoading] = useState(true);
+const MyMemberInviteList = ({daoId}) => {
+    const [offers, setOffers] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const getMyOffers = async () => {
-      try {
-        const offers = await listAllOffers(daoId);
-        setOffers(offers);
-        setLoading(false);
-      } catch (err) {
-        console.log(err);
-        setLoading(false);
-      }
+    useEffect(() => {
+        const getMyOffers = async () => {
+            try {
+                const offers = await listAllOffers(daoId);
+
+                const curAddress = window.starcoin.selectedAddress;
+
+                offers.sort((a: any, b: any) => {
+                    if (a.to_address === curAddress) {
+                        return -1;
+                    }
+                    return 1;
+                })
+
+                setOffers(offers);
+                setLoading(false);
+            } catch (err) {
+                console.log(err);
+                setLoading(false);
+            }
+        };
+
+        getMyOffers();
+    }, [daoId]);
+
+    const handleAccecptOffer = async (_, offer) => {
+        console.log('accepting offer', offer);
+        await doAccecptOffer(daoId);
     };
 
-    getMyOffers();
-  }, [daoId]);
-
-  const handleAccecptOffer = async (_, offer) => {
-    console.log('accepting offer', offer);
-    await doAccecptOffer(daoId);
-  };
-
-  return (
-    <Flex as={Stack} direction='column' spacing={4} w='100%'>
-      {!loading ? (
-        Object.keys(offers).length > 0 ? (
-          Object.values(offers)
-            .slice(0, 10)
-            .map(offer => (
-              <MyMemberInviteCard
-                key={offer.for_user}
-                offer={offer}
-                onAcceptOffer={event =>
-                  handleAccecptOffer(event, offer)
-                }
-              />
-            ))
-        ) : (
-          <Flex mt='100px' w='100%' justify='center'>
-            No Invites Here Yet.
-          </Flex>
-        )
-      ) : (
-        <Spinner size='xl' margin='0 auto' mt='10' />
-      )}
-    </Flex>
-  );
+    return (
+        <Flex as={Stack} direction='column' spacing={4} w='100%'>
+            {!loading ? (
+                Object.keys(offers).length > 0 ? (
+                    Object.values(offers)
+                        .slice(0, 10)
+                        .map(offer => (
+                            <MyMemberInviteCard
+                                key={offer.for_user}
+                                offer={offer}
+                                onAcceptOffer={event =>
+                                    handleAccecptOffer(event, offer)
+                                }
+                            />
+                        ))
+                ) : (
+                    <Flex mt='100px' w='100%' justify='center'>
+                        No Invites Here Yet.
+                    </Flex>
+                )
+            ) : (
+                <Spinner size='xl' margin='0 auto' mt='10'/>
+            )}
+        </Flex>
+    );
 };
 
 export default MyMemberInviteList;
--- plugins/member-proposal-plugin/src/components/myMemberInviteCard.tsx
+++ plugins/member-proposal-plugin/src/components/myMemberInviteCard.tsx
@@ -1,38 +1,49 @@
-import React, { useEffect, useState } from 'react';
-import { Box, Flex, Button, HStack, Stack, Link, Text } from '@chakra-ui/react';
+import React, {useEffect, useState} from 'react';
+import {Box, Flex, Button, HStack, Stack, Link, Text} from '@chakra-ui/react';
 
-const MyMemberInviteCard = ({ offer, onAcceptOffer }) => {
-  return (
-    <Box as={Link} w='60%' p='6' borderWidth='1px' borderColor='whiteAlpha.200' rounded='lg' color='mode.900' isExternal>
-      <Stack spacing={4}>
-        <Flex as={HStack} spacing={2} align='center'>
-          <Text>For:</Text>
-          <Text variant='value'>{offer.for_user}</Text>
-        </Flex>
-        <Flex as={HStack} spacing={2} align='center'>
-          <Text>To Address:</Text>
-          <Text variant='value'>{offer.to_address}</Text>
-        </Flex>
-        <Flex as={HStack} spacing={2} align='center'>
-          <Text>Init SBT:</Text>
-          <Text variant='value'>{offer.init_sbt}</Text>
-        </Flex>
-        <Flex as={HStack} spacing={2} align='center'>
-          <Text>Invite Time:</Text>
-            <Text variant='value'>{new Date(offer.time_lock * 1000).toLocaleString()}</Text>
-        </Flex>
-        <Flex as={HStack} spacing={2} align='center'>
-          <Button
-            size='sm'
-            colorScheme='blue'
-            onClick={onAcceptOffer}
-          >
-            Accept
-          </Button>
-        </Flex>
-      </Stack>
-    </Box>
-  );
+const MyMemberInviteCard = ({offer, onAcceptOffer}) => {
+
+    const [address, setAddress] = useState()
+
+    useEffect(() => {
+        setAddress(window.starcoin.selectedAddress);
+    }, [])
+
+    return (
+        <Box as={Link} w='60%' p='6' borderWidth='1px' borderColor='whiteAlpha.200' rounded='lg' color='mode.900'
+             isExternal>
+            <Stack spacing={4}>
+                <Flex as={HStack} spacing={2} align='center'>
+                    <Text>For:</Text>
+                    <Text variant='value'>{offer.for_user}</Text>
+                </Flex>
+                <Flex as={HStack} spacing={2} align='center'>
+                    <Text>To Address:</Text>
+                    <Text variant='value'>{offer.to_address}</Text>
+                </Flex>
+                <Flex as={HStack} spacing={2} align='center'>
+                    <Text>Init SBT:</Text>
+                    <Text variant='value'>{offer.init_sbt}</Text>
+                </Flex>
+                <Flex as={HStack} spacing={2} align='center'>
+                    <Text>Invite Time:</Text>
+                    <Text variant='value'>{new Date(offer.time_lock * 1000).toLocaleString()}</Text>
+                </Flex>
+                {
+                    address === offer.to_address ?
+                        <Flex as={HStack} spacing={2} align='center'>
+                            <Button
+                                size='sm'
+                                colorScheme='blue'
+                                onClick={onAcceptOffer}
+                            >
+                                Accept
+                            </Button>
+                        </Flex>
+                        : <></>}
+            </Stack>
+        </Box>
+    );
 };
 
 export default MyMemberInviteCard;
